58e20a1ec75c0a8f3cb7c8e4de3e1330e0868e01
Aman Madaan <amn.madaan@gmail.com>
2017-10-29 10:46:37 -0700

    Add multi layered LSTM

--- src/model/name_generator.py
+++ src/model/name_generator.py
@@ -1,19 +1,22 @@
 import tensorflow as tf
-
 from src.features.char_codec import CharCodec
 
 
 class NameGenerator:
 
     """
-    Setup the architecture
+    names: place holder for the names input, (?, max_name_length)
+    next_char_in_name: placeholder for the next character sequence, (? * max_name_length,) where ? is the batch size.
+    n_embeddings: number of hidden sequences in the embeddings.
+    n_lstm_stacks: number of lstms that are stacked before the final output.
+    n_lstm_output_dim: the dimensionality of LSTM's output/hidden state.
     """
-    n_embeddings = 30
-    n_layers = 3
-    n_hidden = 150
 
-    def __init__(self, names, next_char_in_name):
+    def __init__(self, names, next_char_in_name, n_embeddings=30, n_lstm_stacks=2, n_lstm_output_dim=150):
 
+        self.n_embeddings = n_embeddings
+        self.n_lstm_stacks = n_lstm_stacks
+        self.n_lstm_output_dim = n_lstm_output_dim
         """
         Map each sequence of encoded names to a sequence of embeddings.
         """
@@ -23,7 +26,7 @@ class NameGenerator:
             self._embedding_matrix = tf.get_variable(name="matrix",
                                                      initializer=embedding_init) #(vocab_size, n_embeddings)
             self._embedded_names = tf.nn.embedding_lookup(self._embedding_matrix, names,
-                                                          name="embedded_names") #(?, seq_length, n_embeddings)
+                                                          name="embedded_names") #(?, max_name_length, n_embeddings)
 
 
         with tf.variable_scope("lstm"):
@@ -31,8 +34,10 @@ class NameGenerator:
             Next, we define one recurrent cell. The one that takes a vector of dimension n_embeddings as the input and
             generates a vector of size n_hidden as the output. This is for a single step. 
             """
-            lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden) #(1, n_hidden)
-
+            #lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_hidden) #(1, n_hidden)
+            lstm_cell = [tf.nn.rnn_cell.BasicLSTMCell(num_units=self.n_lstm_output_dim)
+                              for layer in range(self.n_lstm_stacks)]
+            multi_lstm_cell = tf.nn.rnn_cell.MultiRNNCell(lstm_cell)
             """
             Dynamic RNN helps us in repeating this LSTM cell as many times as needed. In our case, we need one LSTM 
             cell per character in our name. Thus, we feed in our embedded names vector, which has the
@@ -43,20 +48,24 @@ class NameGenerator:
             - states (?, n_hidden)
             There is one output per lstm_cell, and there is final state.
             """
-            rnn_output_per_step, states = tf.nn.dynamic_rnn(lstm_cell, inputs=self._embedded_names, dtype=tf.float32)
+            #rnn_output_per_step, states = tf.nn.dynamic_rnn(lstm_cell, inputs=self._embedded_names, dtype=tf.float32)
+            lstm_output_per_step, states = tf.nn.dynamic_rnn(multi_lstm_cell, inputs=self._embedded_names,
+                                                            dtype=tf.float32)
+
 
         with tf.variable_scope("dense_layer"):
             """
-            We feed the per step output to a simple dense layer that predicts the next character. There is a bit of an optimization
-            at play here. The RNN we have has max_name_length nodes, each of which generates a vector that has is n_hidden dimensional.
-            We want to feed each of these outputs to a Dense layer, and then generate a logits vector that's vocab_size dimensional.
-            This logits vector is then fed to a softmax to find the winning character. To manage this operation, we first concatenate
-            all the logits across the name. Thus, we get a single tensor that's (batch_size * max_name_length, n_hidden). This is then
-            fed to a dense layer, to generate (batch_size * max_name_length, vocab_size). A reshape takes us back to 
+            We feed the per step output to a simple dense layer that predicts the next character. There is a bit of an
+            optimization at play here. The RNN we have has max_name_length nodes, each of which generates a vector that
+            has is n_hidden dimensional. We want to feed each of these outputs to a Dense layer, and then generate a 
+            logits vector that's vocab_size dimensional. This logits vector is then fed to a softmax to find the winning
+            character. To manage this operation, we first concatenate all the logits across the name. Thus, we get a
+            single tensor that's (batch_size * max_name_length, n_hidden). This is then fed to a dense layer, to
+            generate (batch_size * max_name_length, vocab_size). A reshape takes us back to 
             (batch_size, max_name_length, vocab_size)
             """
-            rnn_output_per_step_stacked = tf.reshape(rnn_output_per_step, shape=[-1, self.n_hidden]) #(?, n_hidden)
-            dense_layer_out_stacked = tf.layers.dense(rnn_output_per_step_stacked, CharCodec.vocab_size) #(?, vocab_size)
+            lstm_output_per_step_stacked = tf.reshape(lstm_output_per_step, shape=[-1, self.n_lstm_output_dim]) #(?, n_hidden)
+            dense_layer_out_stacked = tf.layers.dense(lstm_output_per_step_stacked, CharCodec.vocab_size) #(?, vocab_size)
             predicted_next_char_logits_stacked = tf.reshape(dense_layer_out_stacked,
                                                             shape=[-1, CharCodec.vocab_size]) #(?, vocab_size)
 
@@ -64,7 +73,6 @@ class NameGenerator:
         with tf.variable_scope("loss"):
             self._loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(labels=next_char_in_name,
                                                                                logits=predicted_next_char_logits_stacked))
-
             optimizer = tf.train.AdamOptimizer()
             self._train_op = optimizer.minimize(self.loss)
 
--- src/training/train.py
+++ src/training/train.py
@@ -6,54 +6,63 @@ from src.features.names_dataset import NameGenerationCharLevelDataset
 from src.model.name_generator import NameGenerator
 from tensorflow.contrib.tensorboard.plugins import projector
 import os
+tf.logging.set_verbosity(tf.logging.INFO)
 
-n_epochs = 1000
+LOG_DIR = "../../logs"
+SESSION_DIR = "../../models"
 
-names = tf.placeholder(tf.int32, shape=(None, CharCodec.max_name_length), name="input")
-y = tf.placeholder(dtype=tf.int32, shape=[None])  # (?)
+def train(names_filepaths, model_name, n_epochs=1000):
 
-model = NameGenerator(names=names, next_char_in_name=y)
+    names = tf.placeholder(tf.int32, shape=(None, CharCodec.max_name_length), name="input")
+    y = tf.placeholder(dtype=tf.int32, shape=[None])  # (?)
+    model = NameGenerator(names=names, next_char_in_name=y)
+
+    ##tensorboard
+
+    ### log cross entropy
+    logdir = "{0}/run-{1}-{2}".format(LOG_DIR, model_name, datetime.now().strftime("%Y%m%d%H%M%S"))
+    xentropy_summary = tf.summary.scalar("Cross_Entropy_Loss", model.loss)
+
+    ### plot embeddings
+    """
+    embedding_var = model.char_embedding_matrix
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = embedding_var.name
+    embedding.metadata_path = os.path.join('char_metadata.txt')
+    """
+    file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+    #projector.visualize_embeddings(file_writer, config)
 
 
-model_path = "../../models/caucasian_single_lstm_1000_epochs_6.ckpt"
+    with tf.Session() as sess:
 
-model_name = "single_lstm.ckpt"
+        dataset = NameGenerationCharLevelDataset(names_filepaths)
+        saver = tf.train.Saver()
 
-##tensorboard
+        sess.run(tf.global_variables_initializer())
 
-### log cross entropy
-logdir = "../../logs/run-{0}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
-xentropy_summary = tf.summary.scalar("Cross_Entropy_Loss", model.loss)
+        for i in range(n_epochs):
+            start_time = datetime.now()
+            for j, (features, labels) in enumerate(dataset.iterator(num_batches=20)):
+                 sess.run([model.train_op], feed_dict= { names : features, y: labels})
 
-### plot embeddings
-embedding_var = model.char_embedding_matrix
-config = projector.ProjectorConfig()
-embedding = config.embeddings.add()
-embedding.tensor_name = embedding_var.name
-embedding.metadata_path = os.path.join('char_metadata.txt')
-file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
-#projector.visualize_embeddings(file_writer, config)
+            ls, summary = sess.run([model.loss, xentropy_summary], feed_dict={names: features,   y: labels})
+            print("epoch {0}, loss = {1}, time = {2}".format(i, ls, datetime.now() - start_time))
+            file_writer.add_summary(summary, i)
+            saver.save(sess, os.path.join(logdir, model_name), global_step=i)
 
+        saver.save(sess, "{0}/{1}_{2}_eps".format(SESSION_DIR, model_name, n_epochs))
 
-with tf.Session() as sess:
+    file_writer.close()
 
-    dataset = NameGenerationCharLevelDataset(["../../data/caucasian_names.txt"])
-    saver = tf.train.Saver()
+if __name__ == "__main__":
+    x = {
+        "datasets" : [{
+            "name" : "hispanic",
+            "data" : "../../data/hipanic_names.txt"
+        }]
+    }
 
-    sess.run(tf.global_variables_initializer())
-
-    for i in range(n_epochs):
-        start_time = datetime.now()
-        for j, (features, labels) in enumerate(dataset.iterator(num_batches=20)):
-             sess.run([model.train_op], feed_dict= { names : features, y: labels})
-
-        ls, summary = sess.run([model.loss, xentropy_summary], feed_dict={names: features,   y: labels})
-        print("epoch {0}, loss = {1}, time = {2}".format(i, ls, datetime.now() - start_time))
-
-        #The
-        file_writer.add_summary(summary, i)
-        saver.save(sess, os.path.join(logdir, model_name), global_step=i)
-
-    saver.save(sess, model_path)
-
-file_writer.close()
+    train(names_filepaths=["../../data/hispanic_names.txt"], model_name="hispanic", n_epochs=100)
