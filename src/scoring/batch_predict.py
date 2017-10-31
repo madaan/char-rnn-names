7fd48d63a259322cfe9d2086f5afc97bf712b90b
Aman Madaan <amn.madaan@gmail.com>
2017-10-31 00:14:36 -0700

    Add batch scoring

--- src/scoring/batch_predict.py
+++ src/scoring/batch_predict.py
@@ -7,35 +7,59 @@ import tensorflow as tf
 from src.features.char_codec import CharCodec
 from src.model.name_generator import NameGenerator
 import random
-def predict(test_file, model_path):
+import pandas as pd
+
+def predict(test_seeds, model_name, model_path):
     """
     test_file: file with one name per line
     """
+    tf.reset_default_graph()
     names = tf.placeholder(tf.int32, shape=(None, CharCodec.max_name_length), name="input")
     y = tf.placeholder(dtype=tf.int32, shape=[None])  # (?)
-    model = NameGenerator(names=names, next_char_in_name=y)
+    model = NameGenerator(model_name=model_name, names=names, next_char_in_name=y)
     saver = tf.train.Saver()
+    predictions = []
     with tf.Session() as sess:
         saver.restore(sess, model_path)
-        with open(test_file, "r") as f:
-            for line in f:
-                line = line.strip()
-                #randomly create the seed by taking from 1 character to half of the seed length.
-                seed = line[:random.randint(int(len(line) * 0.3), int(len(line) * 0.5))]
-                res = seed
-                desired_length = 20
-                initial_seed_offset = len(seed)
-                for i in range(desired_length):
-                    feats = CharCodec.encode_and_standardize(res).reshape(1, CharCodec.max_name_length)
-                    prediction = sess.run(model.prediction, feed_dict={names: feats})
-                    res += " ".join(CharCodec.decode(prediction[0])[i + initial_seed_offset - 1])
-                    if res[-1] == CharCodec.NAME_END:
-                        break
-                    #seed = CharCodec.decode(prediction[0])[0]
-                print(line, "|", seed, "|", res)
+        for seed in test_seeds:
+            res = seed
+            initial_seed_offset = len(seed)
+            for i in range(CharCodec.max_name_length - len(seed)):
+                feats = CharCodec.encode_and_standardize(res).reshape(1, CharCodec.max_name_length)
+                prediction = sess.run(model.prediction, feed_dict={names: feats})
+                res += " ".join(CharCodec.decode(prediction[0])[i + initial_seed_offset - 1])
+                if res[-1] == CharCodec.NAME_END:
+                    break
+                #seed = CharCodec.decode(prediction[0])[0]
+            predictions.append(res)
+    return predictions
+
+def get_seeds(names_file):
+    """
+    :param names: a file with one name per line.
+    :return: randomly creates a seed by taking a prefix that's 30% to 50% of the name length.
+    """
+    names, seeds = [], []
+    with open(names_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            for prefix_frac in [0.25, 0.5, 0.75]:
+                names.append(line)
+                seeds.append(line[:int(len(line) * prefix_frac)])
+    return names, seeds
 
 if __name__ == "__main__":
-    predict("../../data/test.txt", model_path="../../models/caucasian_single_lstm.ckpt")
-
-
-
+    races = ["hispanic", "indian", "african_american", "caucasian", "all_races"]
+    #races = ["caucasian"]
+    names, seeds = get_seeds("../../data/test.txt")
+    print(names)
+    print(seeds)
+    res = {}
+    res["names"] = names
+    res["seeds"] = seeds
+    #seeds = ["undertaker"[:i] for i in range(1, 5)]
+    #seeds = ["hash"]
+    for race in races:
+        res[race] = predict(test_seeds=seeds, model_name=race, model_path="../../models/{0}_2000_eps".format(race))
+    pd.DataFrame.from_dict(res).to_csv("../../data/predictions.csv", index=False)
+    print(res)
--- src/training/train.py
+++ src/training/train.py
@@ -1,12 +1,9 @@
-import logging
 from datetime import datetime
 import tensorflow as tf
 from src.features.char_codec import CharCodec
 from src.features.names_dataset import NameGenerationCharLevelDataset
 from src.model.name_generator import NameGenerator
-from tensorflow.contrib.tensorboard.plugins import projector
 import os
-tf.logging.set_verbosity(tf.logging.INFO)
 
 LOG_DIR = "../../logs"
 SESSION_DIR = "../../models"
@@ -22,25 +19,11 @@ def train(names_filepaths, model_name, n_epochs=1000):
     ### log cross entropy
     logdir = "{0}/run-{1}-{2}".format(LOG_DIR, model_name, datetime.now().strftime("%Y%m%d%H%M%S"))
     xentropy_summary = tf.summary.scalar("Cross_Entropy_Loss", model.loss)
-
-    ### plot embeddings
-    """
-    embedding_var = model.char_embedding_matrix
-    config = projector.ProjectorConfig()
-    embedding = config.embeddings.add()
-    embedding.tensor_name = embedding_var.name
-    embedding.metadata_path = os.path.join('char_metadata.txt')
-    """
     file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
 
-    #projector.visualize_embeddings(file_writer, config)
-
-
     with tf.Session() as sess:
-
         dataset = NameGenerationCharLevelDataset(names_filepaths)
         saver = tf.train.Saver()
-
         sess.run(tf.global_variables_initializer())
 
         for i in range(n_epochs):
@@ -48,7 +31,8 @@ def train(names_filepaths, model_name, n_epochs=1000):
             for j, (features, labels) in enumerate(dataset.iterator(num_batches=20)):
                  sess.run([model.train_op], feed_dict= { names : features, y: labels})
 
-            ls, summary = sess.run([model.loss, xentropy_summary], feed_dict={names: features,   y: labels})
+            #get loss for one chunk.
+            ls, summary = sess.run([model.loss, xentropy_summary], feed_dict={names: features, y: labels})
             print("epoch {0}, loss = {1}, time = {2}".format(i, ls, datetime.now() - start_time))
             file_writer.add_summary(summary, i)
             saver.save(sess, os.path.join(logdir, model_name), global_step=i)
@@ -79,4 +63,4 @@ if __name__ == "__main__":
 
     for dataset in datasets["datasets"]:
         print("Processing {0}".format(dataset["name"]))
-        train(names_filepaths=[dataset["data"]], model_name=dataset["name"], n_epochs=1)
+        train(names_filepaths=[dataset["data"]], model_name=dataset["name"], n_epochs=2000)
